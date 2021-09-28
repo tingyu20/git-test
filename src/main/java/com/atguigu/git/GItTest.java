5fc1161c9dde1e59e59f227b0fcfebb360f6022e
che <2252310874@qq.com>
2021-09-28 21:16:48 +0800

    second commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -15,5 +15,7 @@ public class GItTest {
         System.out.println("pull test!");
         System.out.println("gitee test!");
         System.out.println("gitee test2!");
+
+        System.out.println("second test");
     }
 }
